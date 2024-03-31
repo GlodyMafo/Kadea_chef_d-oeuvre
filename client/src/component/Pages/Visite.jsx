c231d96773ade3ff96e28ed0372b0c55af877a47
Glody Mafo <glodymafokis@gmail.com>
2024-03-31 11:58:15 +0200

    adding 3D objet with three js

--- client/src/component/Pages/Visite.jsx
+++ client/src/component/Pages/Visite.jsx
@@ -1,48 +1,118 @@
-import React, { forwardRef, Suspense, useRef } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import {
-  useGLTF,
-  Stage,
-  Preload,
-  PresentationControls,
-  OrbitControls,
-  Stars,
-} from "@react-three/drei";
-import Objet3DD from "../general components/Object3DD";
+// OBJET GLTF
+
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import gsap from "gsap";
 
-function Visite({ props }, ref) {
-  return (
-    
-    <section className="h-screen flex flex-col items-center" ref={ref}>
-      <Canvas
-        style={{
-          position: "absolute",
-          height: "100vh",
-          background: "transparent",
-        }}
-        frameloop="demand"
-        camera={{
-          position: [7.66, 0.24, -1.66],
-          rotation: [-2.75, 1.33, 2.76],
-          fov: [100],
-        }}
-      >
-        <OrbitControls
-          enableZoom={true}
-          enablePan={false}
-          enableDamping
-          dampingFactor={0.2}
-          autoRotate={false}
-          rotateSpeed={-0.5}
-        />
-        <Suspense fallback={null}>
-          <Preload all />
-          <Objet3DD />
-        </Suspense>
-      </Canvas>
-    </section>
-  );
-}
 
-export default forwardRef(Visite);
+const Visite = () => {
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(7.66, 0.24, -1.66);
+    camera.rotation.set(-2.75, 1.33, 2.76);
+
+    scene.add(camera);
+
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    let position = 0;
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load("/scene.gltf", (gltf) => {
+      console.log(gltf);
+      const model = gltf.scene;
+      scene.add(model);
+
+      // Recupérer la position et la rotation de la camera
+      // window.addEventListener("mouseup", function(){
+      //   console.log(camera.position);
+      //   console.log(camera.rotation);
+      // })
+
+      window.addEventListener("mouseup", function () {
+        switch (position) {
+          case 0:
+            cameraMove(-0.49, 1.85, -2.08);
+            cameraRotation(1.26, 1.51, -1.25)
+            position = 1
+            break;
+
+          case 1:
+            cameraMove(-1.52, 0.32, 2.19);
+            cameraRotation(-3.13, 1.31, 3.13)
+            position = 2
+            break;
+
+          case 2:
+            cameraMove(0.10, 1.26, 0.09);
+            cameraRotation(0.12, 0.57, 0.06)
+            position = 3
+            break;
+
+          case 3:
+            cameraMove(-3.34, 9.56, -1.40);
+            cameraRotation(-1.57, -0.68, -1.57)
+            position = 4
+            break;
+        }
+      })
+
+      // GUI pour recupérer les valeurs sur l'écran
+
+      // const Config = new lilGui.GUI();
+
+      // Config
+      //   .add(model.position, 'x').name("model x Axis position").min(-100).max(100).stop(0.001)
+
+      // Config.add(model.position, 'y').name('model y Axis position')
+      // .min(-100)
+      // .max(100)
+      // .stop(0.001)
+
+      // Config.add(model.position, 'z').name('model z Axis position')
+      // .min(-100)
+      // .max(100)
+      // .stop(0.001)
+    });
+
+    function cameraMove(x, y, z) {
+      gsap.to(camera.position, {
+        x,
+        y,
+        z, duration: 3
+      })
+    }
+
+    function cameraRotation(x, y, z) {
+      gsap.to(camera.rotation, {
+        x,
+        y,
+        z,
+        duration: 3,
+      })
+    }
+
+    const animate = () => {
+      renderer.render(scene, camera);
+      // controls.update();
+      window.requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, []);
+
+  return <canvas className="p-20" ref={canvasRef} />;
+};
+
+export default Visite;
